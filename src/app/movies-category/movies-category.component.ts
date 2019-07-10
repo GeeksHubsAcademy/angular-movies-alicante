b2126b1b8a11890e2bb6e92cc181b20279fdb902
jupegarnica <jupegarnica@gmail.com>
2019-07-10 12:59:32 +0200

    category parameter subscribed

--- src/app/movies-category/movies-category.component.ts
+++ src/app/movies-category/movies-category.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-movies-category',
   templateUrl: './movies-category.component.html',
-  styleUrls: ['./movies-category.component.scss']
+  styleUrls: ['./movies-category.component.scss'],
 })
 export class MoviesCategoryComponent implements OnInit {
-
-  constructor() { }
+  category: string;
+  constructor(private route: ActivatedRoute) {}
 
   ngOnInit() {
+    this.route.params.subscribe(params => {
+      this.category = params.category.replace('_',' ');
+    });
   }
-
 }
